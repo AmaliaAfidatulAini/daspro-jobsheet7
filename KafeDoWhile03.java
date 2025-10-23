2e474378a53af2a0af6de880105800be1f295f14
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2025-10-23 08:46:28 +0700

    Percobaan 2.3.1

---
+++ KafeDoWhile03.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class KafeDoWhile03 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int kopi, teh, roti, totalHarga;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 12000;;
+        String namaPelanggan;
+        do{
+            System.out.println("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+            System.out.println("Jumlah kopi:");
+            kopi = sc.nextInt();
+            System.out.println("Jumlah teh:");
+            teh = sc.nextInt();
+            System.out.println("Jumlah roti:");
+            roti = sc.nextInt();
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total harga yang harus dibayar: Rp " + totalHarga);
+            sc.nextLine();
+        } while (true);
+
+        System.out.println("Semua Transaksi Selesai");
+    }
+    
+}
